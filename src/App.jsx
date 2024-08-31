3f90efce89e67063bef0da4f3d5e623988030d0c
EdenDonenfeld <edendonenfeld@gmail.com>
2024-08-31 17:26:02 +0300

    letters on board is working now

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Board from './Components/jsx/Board';
 const App = () => {
 
   const [letter, setLetter] = useState('');
+  const [keyPressCount, setKeyPressCount] = useState(0);
 
   const logLetter = (letter) => {
     console.log(letter);
     setLetter(letter);
+    setKeyPressCount(keyPressCount + 1);
   }
 
   return (
     <div className="App">
       <h1 className="header">Wordle</h1>
-      <Board letter={letter}/>
+      <Board letter={letter} keyPressCount={keyPressCount} />
       <Keyboard onKeyPress={logLetter}/>
     </div>
   );
--- src/Components/jsx/Board.jsx
+++ src/Components/jsx/Board.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Cell from "./Cell";
 import '../css/Board.css';
 
-const Board = ({ letter }) => {
+const Board = ({ letter, keyPressCount }) => {
     const [board, setBoard] = useState(Array(6).fill(null).map(() => Array(5).fill('')));
     const [currentRow, setCurrentRow] = useState(0);
     const [currentCol, setCurrentCol] = useState(0);
@@ -10,41 +10,53 @@ const Board = ({ letter }) => {
     useEffect(() => {
         if (letter) {
             if (letter === 'Enter') {
-                // Check if word is correct
-            }
-            if (letter === 'Del') {
-                console.log('Delete');
-                console.log(currentRow, currentCol);
-                const newBoard = board.map((row, rowIndex) =>
-                    row.map((cell, colIndex) =>
-                        rowIndex === currentRow && colIndex === currentCol-1 ? '' : cell
-                    )
-                );
-                setBoard(newBoard);
-
-                if (currentCol > 0) {
-                    setCurrentCol(currentCol - 1);
-                } else {
-                    setCurrentCol(4);
-                    setCurrentRow(currentRow - 1);
-                }
+                handleEnter();
+            } else if (letter === 'Del') {
+                handleDelete();
             } else {
-                const newBoard = board.map((row, rowIndex) => 
-                    row.map((cell, colIndex) => 
-                        rowIndex === currentRow && colIndex === currentCol ? letter : cell
-                    )
-                );
-                setBoard(newBoard);
-    
-                if (currentCol < 4) {
-                    setCurrentCol(currentCol + 1);
-                } else {
-                    setCurrentCol(0);
-                    setCurrentRow(currentRow + 1);
-                }
+                handleLetterInput(letter);
             }
         }
-    }, [letter]); // eslint-disable-line react-hooks/exhaustive-deps
+    }, [keyPressCount]); // eslint-disable-line react-hooks/exhaustive-deps
+
+
+    const handleEnter = () => {
+        console.log("Current row: ", currentRow);
+        console.log("Current col: ", currentCol);
+        if (currentCol < 5) {
+            console.log("Please fill the entire row before moving to the next row");
+        } else {
+            setCurrentCol(0);
+            setCurrentRow(currentRow + 1);
+        }
+    }
+
+
+    const handleDelete = () => {
+        if (currentCol > 0) {
+            const newBoard = [...board];
+            let newRow = currentRow;
+            let newCol = currentCol;
+            newCol -= 1;
+            
+            newBoard[newRow][newCol] = '';
+            setBoard(newBoard);
+            setCurrentCol(newCol);
+            setCurrentRow(newRow);
+        }
+    };
+
+    const handleLetterInput = (inputLetter) => {
+        if (currentCol < 5 && currentRow < 6) {
+            const newBoard = [...board];
+            newBoard[currentRow][currentCol] = inputLetter;
+            setBoard(newBoard);
+
+            if (currentCol < 5) {
+                setCurrentCol(currentCol + 1);
+            }
+        }
+    };
 
     return (
         <div className="words-container">
