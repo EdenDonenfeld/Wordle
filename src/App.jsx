98c9f9e9a1b97903f07032247ebf9d4f7d1b212d
EdenDonenfeld <edendonenfeld@gmail.com>
2024-09-01 16:37:26 +0300

    added context to solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Keyboard from './Components/jsx/Keyboard';
 import Board from './Components/jsx/Board';
-import { loadWords } from './LoadWords';
+import { SolutionProvider } from './SolutionContext';
 
 const App = () => {
 
   const [letter, setLetter] = useState('');
   const [keyPressCount, setKeyPressCount] = useState(0);
-  const [solution, setSolution] = useState(''); // eslint-disable-line no-unused-vars
-  const [validGuesses, setValidGuesses] = useState([]); // eslint-disable-line no-unused-vars
-  const [validSolutions, setValidSolutions] = useState([]); // eslint-disable-line no-unused-vars
 
-  useEffect(() => {
-    const fetchWords = async () => {
-      const { validGuesses, validSolutions } = await loadWords();
-      setValidGuesses(validGuesses);
-      setValidSolutions(validSolutions);
-      const randomIndex = Math.floor(Math.random() * validSolutions.length);
-      setSolution(validSolutions[randomIndex]);
-      console.log("Solution: ", validSolutions[randomIndex]);
-    };
-    fetchWords();
-  }, []);
-
- 
   const logLetter = (letter) => {
     console.log(letter);
     setLetter(letter);
@@ -32,11 +16,13 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <h1 className="header">Wordle</h1>
-      <Board letter={letter} keyPressCount={keyPressCount} />
-      <Keyboard onKeyPress={logLetter}/>
-    </div>
+    <SolutionProvider>
+      <div className="App">
+        <h1 className="header">Wordle</h1>
+        <Board letter={letter} keyPressCount={keyPressCount} />
+        <Keyboard onKeyPress={logLetter}/>
+      </div>
+    </SolutionProvider>
   );
 }
 
---
+++ src/SolutionContext.jsx
@@ -0,0 +1,34 @@
+import React, {createContext, useState, useEffect, useRef} from 'react';
+import { loadWords } from './LoadWords';
+
+export const SolutionContext = createContext();
+
+export const SolutionProvider = (props) => {
+    const [solution, setSolution] = useState('');
+    const [validGuesses, setValidGuesses] = useState([]);
+    const [validSolutions, setValidSolutions] = useState([]);
+
+    const solutionRef = useRef(solution);
+
+    useEffect(() => {
+        if (!solutionRef.current) {
+          const fetchWords = async () => {
+            const { validGuesses, validSolutions } = await loadWords();
+            setValidGuesses(validGuesses);
+            setValidSolutions(validSolutions);
+            const randomIndex = Math.floor(Math.random() * validSolutions.length);
+            const selectedSolution = validSolutions[randomIndex];
+            setSolution(selectedSolution);
+            console.log(selectedSolution);
+          };
+          fetchWords();
+          solutionRef.current = true;  // Mark words as fetched
+        }
+    }, []);
+
+    return (
+        <SolutionContext.Provider value={{ solution, validGuesses, validSolutions }}>
+            {props.children}
+        </SolutionContext.Provider>
+    );
+}
